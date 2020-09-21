58944acca2bfb312c7e42f378045ba482a121b4a
SemicolonStruggles <38405582+SemicolonStruggles@users.noreply.github.com>
2020-09-21 23:16:44 +0200

    Added automatic grid conversion

--- OSRS HiScore/OSRS_HiScore.py
+++ OSRS HiScore/OSRS_HiScore.py
@@ -6,6 +6,101 @@ from PIL import Image, ImageFont, ImageDraw
 import math
 import json
 
+# TODO: move positions to config
+# TODO: Hiscore show wrong values (fix order or add api id)
+positions = {
+    "Combat": [738, 781, False],
+    "Overall": [800, 781, False],
+    "LMS - Rank": [1120, 781, False],
+    "League Points": [1182, 781, False],
+    "All Clue Scrolls": [1244, 781, False],
+
+    "Beginner Clues": [0, 0, True],
+    "Easy Clues": [0, 1, True],
+    "Medium Clues": [0, 2, True],
+    "Hard Clues": [0, 3, True],
+    "Elite Clues": [0, 4, True],
+    "Master Clues": [0, 5, True],
+    "Bounty Hunter - Hunter": [0, 6, True],
+    "Bounty Hunter - Rogue": [0, 7, True],
+
+    "Attack": [1, 0, True],
+    "Strength": [1, 1, True],
+    "Defence": [1, 2, True],
+    "Ranged": [1, 3, True],
+    "Prayer": [1, 4, True],
+    "Magic": [1, 5, True],
+    "Runecraft": [1, 6, True],
+    "Construction": [1, 7, True],
+
+    "Hitpoints": [2, 0, True],
+    "Agility": [2, 1, True],
+    "Herblore": [2, 2, True],
+    "Thieving": [2, 3, True],
+    "Crafting": [2, 4, True],
+    "Fletching": [2, 5, True],
+    "Slayer": [2, 6, True],
+    "Hunter": [2, 7, True],
+
+    "Mining": [3, 0, True],
+    "Smithing": [3, 1, True],
+    "Fishing": [3, 2, True],
+    "Cooking": [3, 3, True],
+    "Firemaking": [3, 4, True],
+    "Woodcutting": [3, 5, True],
+    "Farming": [3, 6, True],
+    
+    "Abyssal Sire": [4, 0, True],
+    "Alchemical Hydra": [4, 1, True],
+    "Barrows Chests": [4, 2, True],
+    "Bryophyta": [4, 3, True],
+    "Callisto": [4, 4, True],
+    "Cerberus": [4, 5, True],
+    "Chambers of Xeric": [4, 6, True],
+    "Chambers of Xeric: Challenge Mode": [4, 7, True],
+
+    "Chaos Elemental": [5, 0, True],
+    "Chaos Fanatic": [5, 1, True],
+    "Commander Zilyana": [5, 2, True],
+    "Corporal Beast": [5, 3, True],
+    "Crazy Archaeologist": [5, 4, True],
+    "Deranged Archaeologist": [5, 5, True],
+    "Dagannoth Prime": [5, 6, True],
+    "Dagannoth Rex": [5, 7, True],
+    
+    "Dagannoth Supreme": [6, 0, True],
+    "General Graardor": [6, 1, True],
+    "Giant Mole": [6, 2, True],
+    "Grotesque Guardians": [6, 3, True],
+    "Hespori": [6, 4, True],
+    "Kalphite Queen": [6, 5, True],
+    "King Black Dragon": [6, 6, True],
+    "Kraken":[6, 7, True],
+
+    "Kree'Arra": [7, 0, True],
+    "K'ril Tsutsaroth": [7, 1, True],
+    "Mimic": [7, 2, True],
+    "Nightmare": [7, 3, True],
+    "Obor": [7, 4, True],
+    "Sarachnis": [7, 5, True],
+    "Scorpia": [7, 6, True],
+    "Skotizo": [7, 7, True],
+
+    "The Gauntlet": [8, 0, True],
+    "The Corrupted Gauntlet": [8, 1, True],
+    "Theater of Blood": [8, 2, True],
+    "Thermonuclear Smoke Devil": [8, 3, True],
+    "TzKal-Zuk": [8, 4, True],
+    "TzTok-Jad": [8, 5, True],
+    "Venenatis": [8, 6, True],
+    "Vet'ion": [8, 7, True],
+    
+    "Vorkath": [9, 0, True],
+    "Wintertodt": [9, 1, True],
+    "Zalcano": [9, 2, True],
+    "Zulrah": [9, 3, True],
+}
+
 # TODO: Move to helper file
 def getFileContents(path):
     file = open(path, "r")
@@ -17,99 +112,36 @@ def getFileContents(path):
 configPath = Path("./config.json").absolute()
 config = json.loads(getFileContents(configPath))
 
-
 LevelScores = []
 BossScores = []
 # Order of this dictionary must match with API response
 xGridOffset = 685
-yGridOffset = 836
+yGridOffset = 834
 
-# TODO: move positions to config
-Positions = {
-    "Overall": [800, 781],
-    "Attack": [685 + (63 * 1), 836],
-    "Defence": [685 + (63 * 1), 896],
-    "Strength": [685 + (63 * 1), 866],
-    "Hitpoints": [685 + (63 * 2), 836],
-    "Ranged": [685 + (63 * 1), 926],
-    "Prayer": [685 + (63 * 1), 956],
-    "Magic": [685 + (63 * 1), 986],
-    "Cooking": [685 + (63 * 3), 926],
-    "Woodcutting": [685 + (63 * 3), 986],
-    "Fletching": [685 + (63 * 2), 986],
-    "Fishing": [685 + (63 * 3), 896],
-    "Firemaking": [685 + (63 * 3), 956],
-    "Crafting": [685 + (63 * 2), 956],
-    "Smithing": [685 + (63 * 3), 866],
-    "Mining": [685 + (63 * 3), 836],
-    "Herblore": [685 + (63 * 2), 896],
-    "Agility": [685 + (63 * 2), 866],
-    "Thieving": [685 + (63 * 2), 926],
-    "Slayer": [685 + (63 * 2), 1016],
-    "Farming": [685 + (63 * 3), 1016],
-    "Runecraft": [685 + (63 * 1), 1016],
-    "Hunter": [685 + (63 * 2), 1046],
-    "Construction": [685 + (63 * 1), 1046],
-    "League Points": [1182, 781],
-    "Bounty Hunter - Hunter": [685 + (63 * 0), 1016],
-    "Bounty Hunter - Rogue": [685 + (63 * 0), 1046],
-    "All Clue Scrolls": [1244, 781],
-    "Beginner Clues": [685, 836],
-    "Easy Clues": [685 + (63 * 0), 866],
-    "Medium Clues": [685 + (63 * 0), 896],
-    "Hard Clues": [685 + (63 * 0), 926],
-    "Elite Clues": [685 + (63 * 0), 956],
-    "Master Clues": [685 + (63 * 0), 986],
-    "LMS - Rank": [1120, 781],
-    "Abyssal Sire": [685 + (63 * 4), 836],
-    "Alchemical Hydra": [685 + (63 * 4), 866],
-    "Barrows Chests": [685 + (63 * 4), 896],
-    "Bryophyta": [685 + (63 * 4), 926],
-    "Callisto": [685 + (63 * 4), 956],
-    "Cerberus": [685 + (63 * 4), 986],
-    "Chambers of Xeric": [685 + (63 * 4), 1016],
-    "Chambers of Xeric: Challenge Mode": [685 + (63 * 4), 1046],
-    "Chaos Elemental": [685 + (63 * 5), 836],
-    "Chaos Fanatic": [685 + (63 * 5), 866],
-    "Commander Zilyana": [685 + (63 * 5), 896],
-    "Corporal Beast": [685 + (63 * 6), 836],
-    "Crazy Archaeologist": [685 + (63 * 5), 926],
-    "Dagannoth Prime": [685 + (63 * 5), 1016],
-    "Dagannoth Rex": [685 + (63 * 5), 1046],
-    "Dagannoth Supreme": [685 + (63 * 5), 986],
-    "Deranged Archaeologist": [685 + (63 * 5), 956],
-    "General Graardor": [685 + (63 * 6), 866],
-    "Giant Mole": [685 + (63 * 6), 896],
-    "Grotesque Guardians": [685 + (63 * 6), 926],
-    "Hespori": [685 + (63 * 6), 956],
-    "Kalphite Queen": [685 + (63 * 6), 986],
-    "King Black Dragon": [685 + (63 * 6), 1016],
-    "Kraken": [685 + (63 * 6), 1046],
-    "Kree'Arra": [685 + (63 * 7), 836],
-    "K'ril Tsutsaroth": [685 + (63 * 7), 866],
-    "Mimic": [685 + (63 * 7), 896],
-    "Nightmare": [685 + (63 * 7), 926],
-    "Obor": [685 + (63 * 7), 956],
-    "Sarachnis": [685 + (63 * 7), 986],
-    "Scorpia": [685 + (63 * 7), 1016],
-    "Skotizo": [685 + (63 * 7), 1046],
-    "The Gauntlet": [685 + (63 * 8), 836],
-    "The Corrupted Gauntlet": [685 + (63 * 8), 866],
-    "Theater of Blood": [685 + (63 * 8), 896],
-    "Thermonuclear Smoke Devil": [685 + (63 * 8), 926],
-    "TzKal-Zuk": [685 + (63 * 8), 956],
-    "TzTok-Jad": [685 + (63 * 8), 986],
-    "Venenatis": [685 + (63 * 8), 1016],
-    "Vet'ion": [685 + (63 * 8), 1046],
-    "Vorkath": [685 + (63 * 9), 836],
-    "Wintertodt": [685 + (63 * 9), 866],
-    "Zalcano": [685 + (63 * 9), 896],
-    "Zulrah": [685 + (63 * 9), 926],
-    "Combat": [738, 781]
-}
+def calculatePositions(positions):
+    # todo: get spokes from config
+    xSpokes = [0, 63, 126, 189, 252, 315, 376, 439, 502, 565]
+    ySpokes = [0, 30, 60, 90, 120, 150, 180, 210]
+
+    for position in positions:
+        positionData = positions.get(position)
+        x = positionData[0]
+        y = positionData[1]
+        usesGrid = positionData[2]
+        
+        if usesGrid:
+            print(f"Using grid position for {position}")
+            x = xSpokes[x] + xGridOffset
+            y = ySpokes[y] + yGridOffset
+
+            positions.update({position: [x, y, True]})
+
+            print(f"Calculated grid positions at x={x} y={y}")
+            print("")
 
 accounts = config.get("accounts")
 accountTypeParameters = config.get("accountTypeParameters")
+calculatePositions(positions)
 
 class Position:
     def __init__(self, x, y):
@@ -144,21 +176,21 @@ class BossData:
 def processHiscoreItem(itemData, itemCount):
     itemData = itemData.split(',')
     for i in range(0, len(itemData)):
-        print(itemData[i])
+        # print(itemData[i])
         if itemData[i] == "-1":
             itemData[i] = '-'
     # TODO: skip list creation and take directly from dict
-    itemName = list(Positions.keys())[itemCount]
+    itemName = list(positions.keys())[itemCount]
 
     if len(itemData) == 2:
         # Hiscore entry is boss
         bossScore = BossData(itemData, itemName)
-        print(repr(bossScore))
+        # print(repr(bossScore))
         BossScores.append(bossScore)
     elif len(itemData) == 3:
         # Hiscore entry is level
         levelScore = LevelData(itemData, itemName)
-        print(repr(levelScore))
+        # print(repr(levelScore))
         LevelScores.append(levelScore)
 
 
@@ -176,8 +208,8 @@ def processPlayerHiscore(response):
 
 
 def getPosition(name):
-    x = Positions[name][0]
-    y = Positions[name][1]
+    x = positions[name][0]
+    y = positions[name][1]
     return Position(x, y)
 
 
@@ -223,19 +255,6 @@ font = ImageFont.truetype("{}\\Reckoner.ttf".format(
 
 # get the line size
 text_width, text_height = font.getsize(unicode_text)
-
-# TODO: remove this
-
-Accounts_old = ['Mystic Blade', 'Xeric Blade', 'Epic Blade', 'Magic Blade', 'Garlic Blade', 'Cyclic Blade', 'Irenic Blade', 'Lyric Blade', 'Agonic Blade', 'Arabic Blade', 'Auric Blade', 'Azoic Blade', 'Azotic Blade', 'Bardic Blade', 'Baric Blade', 'Boric Blade', 'Bromic Blade', 'Cadmic Blade', 'Calcic Blade', 'Ceric Blade', 'Citric Blade', 'Cleric Blade', 'Cultic Blade', 'Cupric Blade', 'Cyanic Blade', 'Dyadic Blade', 'Emic Blade', 'Erotic Blade', 'Ethnic Blade', 'Etic Blade', 'Felsic Blade', 'Gnomic Blade', 'Holmic Blade', 'Humic Blade', 'Iodic Blade', 'Ionic Blade', 'Iridic Blade', 'Laic Blade', 'Lithic Blade', 'Logic Blade', 'Mafic Blade', 'Mantic Blade', 'Melic Blade', 'Mimic Blade', 'Music Blade', 'Niobic Blade', 'Nitric Blade', 'Odic Blade', 'Ontic Blade', 'Orphic Blade',
-                'Osmic Blade', 'Oxalic Blade', 'Oxidic Blade', 'Ozonic Blade', 'Panic Blade', 'Photic Blade', 'Poetic Blade', 'Rhodic Blade', 'Sodic Blade', 'Steric Blade', 'Telic Blade', 'Terbic Blade', 'Thic Blade', 'Thoric Blade', 'Toluic Blade', 'Typic Blade', 'Uranic Blade', 'Vitric Blade', 'Yttric Blade', 'Zincic Blade', 'Mastic Blade', 'Pyric Blade', 'Pelvic Blade', 'Daric Blade', 'Sepic Blade', 'Limbic Blade', 'Picnic Blade', 'Mesic Blade', 'Civic Blade', 'Ethic Blade', 'Medic Blade', 'Ovonic Blade', 'Azonic Blade', 'Chemic Blade', 'Echoic Blade', 'Fustic Blade', 'Toric Blade', 'Conic Blade', 'Hemic Blade', 'Salic Blade', 'Biotic Blade', 'Hydric Blade', 'Axenic Blade', 'Syndic Blade', 'Critic Blade', 'Frolic Blade', 'Fabric Blade', 'Zoic Blade', 'Emetic Blade', 'Antic Blade', 'Ludic Blade']
-HCIMAccounts = ['Heroic Blade']
-UIMAccounts = ['BladeBTW']
-GIMAccounts = ['Edenic Blade', 'Deific Blade',
-               'Ferric Blade', 'Exilic Blade', 'Orphic Blade']
-IMAccounts = []
-DeadmanModeAccounts = []
-LeaguesAccounts = []
-Tournamentaccounts = []
 
 HiScoreList = ["Overall", "Attack", "Defence", "Strength", "Hitpoints", "Ranged", "Prayer", "Magic", "Cooking", "Woodcutting", "Fletching", "Fishing", "Firemaking", "Crafting", "Smithing", "Mining", "Herblore", "Agility", "Thieving", "Slayer", "Farming", "Runecraft", "Hunter", "Construction", "League Points", "Bounty Hunter - Hunter", "Bounty Hunter - Rogue", "All Clue Scrolls", "Beginner Clues", "Easy Clues", "Medium Clues", "Hard Clues", "Elite Clues", "Master Clues", "LMS - Rank", "Abyssal Sire", "Alchemical Hydra", "Barrows Chests", "Bryophyta", "Callisto", "Cerberus", "Chambers of Xeric",
                "Chambers of Xeric: Challenge Mode", "Chaos Elemental", "Chaos Fanatic", "Commander Zilyana", "Corporal Beast", "Crazy Archaeologist", "Dagannoth Prime", "Dagannoth Rex", "Dagannoth Supreme", "Deranged Archaeologist", "General Graardor", "Giant Mole", "Grotesque Guardians", "Hespori", "Kalphite Queen", "King Black Dragon", "Kraken", "Kree'Arra", "K'ril Tsutsaroth", "Mimic", "Nightmare", "Obor", "Sarachnis", "Scorpia", "Skotizo", "The Gauntlet", "The Corrupted Gauntlet", "Theater of Blood", "Thermonuclear Smoke Devil", "TzKal-Zuk", "TzTok-Jad", "Venenatis", "Vet'ion", "Vorkath", "Wintertodt", "Zalcano", "Zulrah", "Combat"]
@@ -283,67 +302,37 @@ def loopthroughAccounts():
 
         # TODO: Leagues - unsure what old code function was. See line 103 in base repo
 
-        if len(HiScoreDictionary) > 0:
-            Numeric = True
-            for key in HiScoreDictionary.values():
-                if key == '':
-                    Numeric = False
-                HiscoreKeyList = []
-                if ',' in key:
-                    HiscoreKeyList = key.split(',')
-                    for key in HiscoreKeyList:
-                        if key.isnumeric() == False:
-                            Numeric = False
-            # print('Numeric = {}'.format(Numeric))
-            if Numeric == True:
-                # print('Numeric = {}'.format(Numeric))
-                print('HiScoreDictionary = {}'.format(HiScoreDictionary))
-                stats = [float(HiScoreDictionary['Attack'].split(',')[1]), float(HiScoreDictionary['Hitpoints'].split(',')[1]), float(HiScoreDictionary['Strength'].split(',')[1]),
-                         float(HiScoreDictionary['Defence'].split(',')[1]), float(
-                             HiScoreDictionary['Ranged'].split(',')[1]), float(HiScoreDictionary['Prayer'].split(',')[1]),
-                         float(HiScoreDictionary['Magic'].split(',')[1])]
-                if stats[0] == (float(-1.0)):
-                    # print('stats[0]==float(-1.0))')
-                    combatlvl = '-'
-                else:
-                    # print('combatlvl calced through combat_level(stats)')
-                    combatlvl = combat_level(stats)
-                    # print('combatlvl = {}'.format(combatlvl))
-                temp_list.append('{},{},{}'.format(
-                    combatlvl, combatlvl, combatlvl))
-        HiScoreDictionary = dict(zip(HiScoreList, temp_list))
+        # TODO: Combat level calculation
 
         font = ImageFont.truetype("{}\\Reckoner.ttf".format(
             absolutePath), 14, encoding="unic")
-        d = ImageDraw.Draw(im)
+        drawImg = ImageDraw.Draw(baseImage)
         text_color = (255, 255, 255)
 
         for levelScore in LevelScores:
             text = levelScore.level
             x = levelScore.Position.x
             y = levelScore.Position.y
-            print(d.textsize(text, font=font))
-            width, height = d.textsize(text, font=font)
+            # print(drawImg.textsize(text, font=font))
+            width, height = drawImg.textsize(text, font=font)
 
-            print(
-                f"Drew skill: {levelScore.name} at X={levelScore.Position.x} and Y={levelScore.Position.y}")
+            print(f"Drew skill: {levelScore.name} at X={levelScore.Position.x} and Y={levelScore.Position.y}")
 
-            d.text((x, y), text, font=font, fill=text_color)
+            drawImg.text((x, y), text, font=font, fill=text_color)
 
         for bossScore in BossScores:
             text = bossScore.kills
             x = bossScore.Position.x
             y = bossScore.Position.y
-            print(d.textsize(text, font=font))
-            width, height = d.textsize(text, font=font)
+            # print(drawImg.textsize(text, font=font))
+            width, height = drawImg.textsize(text, font=font)
 
-            print(
-                f"Drew boss: {bossScore.name} at X={bossScore.Position.x} and Y={bossScore.Position.y}")
+            # print(f"Drew boss: {bossScore.name} at X={bossScore.Position.x} and Y={bossScore.Position.y}")
 
-            d.text((x, y), text, font=font, fill=text_color)
+            drawImg.text((x, y), text, font=font, fill=text_color)
 
         print(f"{outputPath}/{account}_hiscore.png")
-        im.save(f"{outputPath}/{account}_hiscore.png")
+        baseImage.save(f"{outputPath}/{account}_hiscore.png")
 
         # print("{}{}_hiscore.png".format(outputPath, account))
         # TODO: remove exit()
@@ -352,4 +341,5 @@ def loopthroughAccounts():
 
 
 while True:
+    # TODO: add update interval/delay
     loopthroughAccounts()
